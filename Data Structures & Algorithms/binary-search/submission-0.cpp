91644ae086931a8c14e5b8848c5e7de93b7cf918
Meet Rupesh Gandhi <161362677+meet-gandhi-1801@users.noreply.github.com>
2026-06-27 17:00:41 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int h = nums.size() - 1;
+        while(l <= h)
+        {
+            int mid = (l + h) >> 1;
+            if(nums[mid] == target)  return mid;
+            else if (nums[mid] > target) h = mid-1;
+            else l = mid + 1;
+        }
+
+        return -1;
+        
+    }
+};
