42d099d092b1d161daaf1513e3cabdf70baf2cef
Meet Rupesh Gandhi <161362677+meet-gandhi-1801@users.noreply.github.com>
2026-07-02 00:57:51 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    vector<vector<int>> ans;
+
+    void solve(int i , vector<int> &nums)
+    {
+        if(i == nums.size())
+        {
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int j = i;j<nums.size();j++)
+        {
+            swap(nums[i],nums[j]);
+            solve(i+1,nums);
+            swap(nums[i],nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        solve(0,nums);
+        return ans;
+    }
+};
