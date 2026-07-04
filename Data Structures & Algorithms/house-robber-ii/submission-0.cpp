ebb5ff26a94f1280f06f3e6430528ce4f5d1f264
Meet Rupesh Gandhi <161362677+meet-gandhi-1801@users.noreply.github.com>
2026-07-05 01:48:16 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int l, int r) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = l; i <= r; i++) {
+            int cur = max(prev1, nums[i] + prev2);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
+            return nums[0];
+
+        return max(
+            solve(nums, 0, n - 2),
+            solve(nums, 1, n - 1)
+        );
+    }
+};
